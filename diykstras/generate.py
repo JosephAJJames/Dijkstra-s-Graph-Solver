7543fefd54847feec7dd53fe34e0a0f1d71b68f7
Joseph James <Joe.Centurion.5@gmail.com>
2024-04-20 21:10:37 +0100

    Made new method to generate random number

--- diykstras/generate.py
+++ diykstras/generate.py
@@ -5,6 +5,7 @@ class GenerateGraph:
     def __init__(self):
         self.dict = {}
         self.nodes = []
+        self.graphSize = None
         pass
 
     def clearDict(self):
@@ -12,16 +13,24 @@ class GenerateGraph:
         pass
 
     def generateNodes(self):
-        self.nodes.append("A") #ensure A (which is the start point) is always in the graph
-        nodeChoices= ["B", "C", "D", "E", "F", "G", "H", "I"]
-        indiciesUsed = [] #list of indicies used to avoid repeated nodes
-        num = random.randint(0, len(indiciesUsed) - 1)
-        if num in indiciesUsed:
-            while (num in indiciesUsed):
-                num = random.randint(0, len(indiciesUsed) - 1)
-    
-        newNode = nodeChoices[num]
-        self.nodes.append(newNode)
+        graphSize = random.randint(5, 8) #determines the size of graph
+        iteration = 0
+        while (iteration <= graphSize):
+            self.nodes.append("A") #ensure A (which is the start point) is always in the graph
+            nodeChoices= ["B", "C", "D", "E", "F", "G", "H", "I"]
+            indiciesUsed = [] #list of indicies used to avoid repeated nodes
+            num = self.generateUnique(indiciesUsed, len(nodeChoices) - 1)
+            newNode = nodeChoices[num]
+            self.nodes.append(newNode)
+            iteration =+ iteration
+        pass
+
+    def generateUnique(self, numList, length):
+        num = random.randint(0, length)
+        while (num in numList):
+            num = random.randint(0, length)
+            
+        return num
         pass
 
     def createWeightings(self):
