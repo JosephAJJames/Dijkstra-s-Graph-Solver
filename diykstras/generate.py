95851ef01ab35b8e4e96a5e131108317d9f132ca
Joseph James <Joe.Centurion.5@gmail.com>
2024-04-22 22:18:08 +0100

    Fixed Bugs

--- diykstras/generate.py
+++ diykstras/generate.py
@@ -5,33 +5,51 @@ class GenerateGraph:
     def __init__(self):
         self.dict = {}
         self.nodes = []
-        self.graphSize = None
+        self.graphSizeMax = 8
         pass
 
     def clearDict(self):
         self.dict = {}
+        self.nodes = []
+        pass
+
+    def printNodes(self):
+        print(self.nodes)
         pass
 
     def generateNodes(self):
-        graphSize = random.randint(5, 8) #determines the size of graph
+        graphSize = random.randint(5, self.graphSizeMax) #determines the size of graph(between 5 to 8)
         iteration = 0
+        self.nodes.append("A") #ensure A (which is the start point) is always in the graph
+        indiciesUsed = [] #list of indicies used to avoid repeated nodes
         while (iteration <= graphSize):
-            self.nodes.append("A") #ensure A (which is the start point) is always in the graph
             nodeChoices= ["B", "C", "D", "E", "F", "G", "H", "I"]
-            indiciesUsed = [] #list of indicies used to avoid repeated nodes
-            num = self.generateUnique(indiciesUsed, len(nodeChoices) - 1)
+            num, numList = self.generateUnique(indiciesUsed, len(nodeChoices) - 1)
+            indiciesUsed = numList
             newNode = nodeChoices[num]
             self.nodes.append(newNode)
-            iteration =+ iteration
+            iteration = iteration + 1
         pass
 
     def generateUnique(self, numList, length):
         num = random.randint(0, length)
-        while (num in numList):
-            num = random.randint(0, length)
-            
-        return num
+        if num in numList:
+            while (num in numList):
+                num = random.randint(0, length)
+                #print("stuck in small loop")
+        numList.append(num)
+        return num, numList
         pass
 
+    def createConnections(self):
+
+        pass
+    
     def createWeightings(self):
         pass
+
+graphGenerator = GenerateGraph()
+for x in range(0, 4):
+    graphGenerator.generateNodes()
+    graphGenerator.printNodes()
+    graphGenerator.clearDict()
